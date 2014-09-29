c015546fffda766e4a9398e2d7e7ef79761f9aa0
Sleepy Daddy Software™ <bell.jeremy@gmail.com>
2014-09-28 23:04:28 -0400

    Added new type declarations for the AudioReflector and the Overlays globals. Ported audioReflectorTools.js to typescript.

--- HiFi/hifi/Audio.d.ts
+++ HiFi/hifi/Audio.d.ts
@@ -79,6 +79,157 @@ declare module hifi {
          */
         setInputVolume(volume: number): void;
     }
+
+    /**
+     * checked against /interface/src/AudioReflector.h
+     * 2014-09-28
+     */
+    interface IAudioReflector {
+        /** @returns int */
+        getReflections(): number;
+        /** @returns float */
+        getAverageDelayMsecs(): number;
+        /** @returns float */
+        getAverageAttenuation(): number;
+        /** @returns float */
+        getMaxDelayMsecs(): number;
+        /** @returns float */
+        getMaxAttenuation(): number;
+        /** @returns float */
+        getMinDelayMsecs(): number;
+        /** @returns float */
+        getMinAttenuation(): number;
+        /** @returns float */
+        getDelayFromDistance(): number;
+        /** @returns int */
+        getDiffusionPathCount(): number;
+        /** @returns int */
+        getEchoesInjected(): number;
+        /** @returns int */
+        getEchoesSuppressed(): number;
+
+        /**
+         * ms of delay added to all echos
+         * @returns float
+         */
+        getPreDelay(): number;
+        /**
+         * ms of delay added to all echos
+         * @param preDelay float
+         */
+        setPreDelay(preDelay: number): void;
+
+        /**
+         * ms per meter that sound travels, larger means slower, which sounds bigger
+         * @returns float
+         */
+        getSoundMsPerMeter(): number;
+        /**
+         * ms per meter that sound travels, larger means slower, which sounds bigger
+         * @param soundsMsPerMeter float
+         */
+        setSoundMsPerMeter(soundsMsPerMeter: number): void;
+
+        /**
+         * scales attenuation to be louder or softer than the default distance attenuation
+         * @returns float
+         */
+        getDistanceAttenuationScalingFactor(): number;
+        /**
+         * scales attenuation to be louder or softer than the default distance attenuation
+         * @param factor float
+         */
+        setDistanceAttenuationScalingFactor(factor: number): void;
+
+        /**
+         * scales attenuation of local audio to be louder or softer than the default attenuation
+         * @returns float
+         */
+        getLocalAudioAttenuationFactor(): number;
+        /**
+         * scales attenuation of local audio to be louder or softer than the default attenuation
+         * @param factor float
+         */
+        setLocalAudioAttenuationFactor(factor: number): void;
+
+        /**
+         * ms window in which we will suppress echoes to reduce comb filter effects
+         * @return float
+         */
+        getCombFilterWindow(): number;
+        /**
+         * ms window in which we will suppress echoes to reduce comb filter effects
+         * @param value float
+         */
+        setCombFilterWindow(value: number): void;
+
+        /**
+         * number of points of diffusion from each reflection point, as fanout increases there are more chances for secondary
+         * echoes, but each diffusion ray is quieter and therefore more likely to be below the sound floor
+         * @returns int
+         */
+        getDiffusionFanout(): number;
+        /**
+         * number of points of diffusion from each reflection point, as fanout increases there are more chances for secondary
+         * echoes, but each diffusion ray is quieter and therefore more likely to be below the sound floor
+         * @param fanout int
+         */
+        setDiffusionFanout(fanout: number): void;
+
+        /**
+         * ratio 0.0 - 1.0 of amount of each ray that is absorbed upon hitting a surface  
+         * @returns float
+         */
+        getAbsorptionRatio(): number;
+        /**
+         * ratio 0.0 - 1.0 of amount of each ray that is absorbed upon hitting a surface  
+         * @param ratio float
+         */
+        setAbsorptionRatio(ratio: number): void;
+
+        /**
+         * ratio 0.0 - 1.0 of amount of each ray that is diffused upon hitting a surface
+         * @return float
+         */
+        getDiffusionRatio(): number;
+        /**
+         * ratio 0.0 - 1.0 of amount of each ray that is diffused upon hitting a surface
+         * @param ratio float
+         */
+        setDiffusionRatio(ratio: number): void;
+
+        /**
+         * remaining ratio 0.0 - 1.0 of amount of each ray that is cleanly reflected upon hitting a surface
+         * @returns float
+         */
+        getReflectiveRatio(): number;
+        /**
+         * remaining ratio 0.0 - 1.0 of amount of each ray that is cleanly reflected upon hitting a surface
+         * @param ratio float
+         */
+        setReflectiveRatio(ratio: number): void;
+
+        /**
+         * wet/dry mix - these don't affect any reflection calculations, only the final mix volumes
+         * @returns float
+         */
+        getOriginalSourceAttenuation(): number;
+        /**
+         * wet/dry mix - these don't affect any reflection calculations, only the final mix volumes
+         * @param value float
+         */
+        setOriginalSourceAttenuation(value: number): void;
+        /**
+         * wet/dry mix - these don't affect any reflection calculations, only the final mix volumes
+         * @returns float
+         */
+        getEchoesAttenuation(): number;
+        /**
+         * wet/dry mix - these don't affect any reflection calculations, only the final mix volumes
+         * @param value float
+         */
+        setEchoesAttenuation(value: number): void;
+    }
 }
 
 declare var AudioInjectionOptions: {
@@ -106,3 +257,4 @@ declare var Sound: {
 
 declare var Audio: hifi.IAudioScriptingInterface;
 declare var AudioDevice: hifi.IAudioDeviceScriptingInterface;
+declare var AudioReflector: hifi.IAudioReflector;
---
+++ HiFi/hifi/Overlays.d.ts
@@ -0,0 +1,58 @@
+﻿/// <reference path="IVec2.d.ts" />
+/// <reference path="IColor.d.ts" />
+declare module hifi {
+
+    /**
+     * Checked against /interface/src/ui/overlays/Overlay.cpp
+     * see Overlay::setProperties
+     * 2014-09-28
+     */
+    interface IOverlayProperties {
+        color?: IColor;
+        /** float */
+        alpha?: number;
+        visible?: boolean;
+        anchor?: string;
+    }
+
+    /**
+     * Checked against interface/src/ui/overlays/Overlays.{h,cpp}
+     * 2014-09-28
+     */
+    interface IOverlays {
+        /**
+         * adds an overlay with the specific properties
+         * @param type valid values: image, text, cube, sphere,
+         * line3d, localvoxels, localmodels, model, billboard
+         * @returns unsigned int
+         */
+        addOverlay(type: string, properties: IOverlayProperties): number;
+
+        /**
+         * edits an overlay updating only the included properties, will return the identified OverlayID in case of
+         * successful edit, if the input id is for an unknown overlay this function will have no effect
+         * @param id unsigned int
+         */
+        editOverlay(id: number, properties: IOverlayProperties): boolean;
+
+        /**
+         * deletes a particle
+         * @param id unsigned int
+         */
+        deleteOverlay(id: number): void;
+
+        /**
+         * returns the top most overlay at the screen point, or 0 if not overlay at that point
+         * @returns unsigned int
+         */
+        getOverlayAtPoint(point: IVec2): number;
+
+        /**
+         * returns whether the overlay's assets are loaded or not
+         * @param id unsigned int
+         */
+        isLoaded(id: number): boolean;
+    }
+}
+
+declare var Overlays: hifi.IOverlays;
